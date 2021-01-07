9ece266bdd9a7af1bf5ae5fb8887873631c24981
Yuhigawa <pablostreiski@outlook.com>
2021-01-06 21:48:50 -0300

    Initial Commit

---
+++ nextjsapp/README.md
@@ -0,0 +1,11 @@
+### Simple Next.js Demo
+
+```bash
+
+> git clone 
+> npm install
+> npm run dev
+
+```
+
+then goes to ``/cars``.
---
+++ nextjsapp/pages/cars/index.js
@@ -0,0 +1,18 @@
+export default function Car() {
+    return (
+    <>
+        <center>
+
+            <h1> <strong> Cars List </strong> </h1>
+
+            <h3> Click in one of those:  </h3>
+
+            <div>
+                <a href="cars/fambo"> Fambo | </a>
+                <a href="cars/lambo"> Lambo | </a>
+                <a href="cars/tesla"> Tesla </a>
+            </div>
+        </center>
+    </>
+    );
+}
